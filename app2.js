e53f0675bdc6c646edccafe2839a8e6255faac35
Rasmus Jannerup <rasm211d@edu.easj.dk>
2020-09-10 14:13:46 +0200

    Init

---
+++ app2.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const hostname = 'localhost';
+const port = '3000';
+
+const server = http.createServer(function(req, res){
+    let root = path.resolve('./wwwroot');
+    if (req.method !== "GET") {
+        res.statusCode = 405;
+        res.end();
+    } else if (req.url === "/") {
+        res.writeHead(302, {Location: "/index.html"});
+        res.end();
+    } else {
+        if (fs.existsSync(path.join(root, req.url))) {
+            res.statusCode = 200;
+            res.write(fs.readFileSync(path.join(root, req.url)));
+            res.end();
+        } else {
+            res.statusCode = 404;
+            res.end();
+        }
+    }
+}) 
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static("wwwroot"));
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
