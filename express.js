3880efb395f4da66bed44e65641c6b5c59247730
Rasmus Sloth Jensen <rsj@tat.dev>
2020-09-10 15:24:59 +0200

    wip

--- express.js
+++ express.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-app.use('/', (res, resp) => {
-  resp.write('Hello world!');
-  resp.end();
-});
+app.use(express.static("wwwroot"));
 
 
 
